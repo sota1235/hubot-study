db0965fe79a7ff0486b7362396c5f51d63d68b17
sota1235 <sota1235@gmail.com>
2016-11-30 18:19:27 +0900

    modify: add hear sample

--- scripts/example.js
+++ scripts/example.js
@@ -5,7 +5,17 @@
 //   hubot ping -- return pong
 
 module.exports = function (robot) {
+  /**
+   * hubot {command}の実装例
+   */
   robot.respond(/ping/i, function (msg) {
     msg.send('PONG');
+  });
+
+  /**
+   * hearの実装例
+   */
+  robot.hear(/おはようhubot/i, function (msg) {
+    msg.send('おはよう');
   });
 };
