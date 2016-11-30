b14909195fd8b55d3e2d5bd25bda4b8f032b2005
sota1235 <sota1235@gmail.com>
2016-11-30 18:17:17 +0900

    modify: add hubot-help

--- scripts/example.js
+++ scripts/example.js
@@ -2,8 +2,7 @@
 //   この部分がhubot helpの説明に出るよ
 //
 // Commands:
-//   ping -- return pong
-
+//   hubot ping -- return pong
 
 module.exports = function (robot) {
   robot.respond(/ping/i, function (msg) {
