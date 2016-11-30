1ceb34a4dfb30d0a35abd6e4d1b682bbf2a0d664
sota1235 <sota1235@gmail.com>
2016-11-30 18:22:21 +0900

    modify: add sample to get data from callback function's argument

--- scripts/example.js
+++ scripts/example.js
@@ -18,4 +18,16 @@ module.exports = function (robot) {
   robot.hear(/おはようhubot/i, function (msg) {
     msg.send('おはよう');
   });
+
+  /**
+   * msgからいろんな情報を取り出す例
+   */
+  robot.respond(/whoami/i, function (msg) {
+    // 発言された部屋
+    room = msg.message.room
+    // 発言したユーザー
+    user = msg.message.user.name
+
+    msg.send('あなたの名前は' + user + 'です');
+  });
 };
